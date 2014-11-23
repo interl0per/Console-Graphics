0c90ef7ed7c02744be130b22d221b4b2126ef324
interl0per <cobatltking6@gmail.com>
2014-11-23 16:58:22 -0500

    Update ASCIIRender.cs

--- ASCIIRender.cs
+++ ASCIIRender.cs
@@ -1,10 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.IO;
+
 namespace ConsoleGraphics
 {
     class ASCIIRender
@@ -16,21 +15,20 @@ namespace ConsoleGraphics
 
         public static void bt()
         {
-            StreamReader r = new StreamReader("levels.txt");
-            levels = new char[256];
+            image1 = (Bitmap)Image.FromFile(@"Lenna.bmp", true);//load character set (digits 1->9..)
             int index = 0;
+            const int WIDTH = 1;
+            const int HEIGHT = 1;
+            bitmapColorsCached = new byte[image1.Width, image1.Height];
+            levels = new char[256];
+
+            StreamReader r = new StreamReader("levels.txt");
             while (!r.EndOfStream && index < 255)
             {
                 string e1 = r.ReadLine();
                 levels[index] = Char.Parse(e1);
                 index++;
             }
-            image1 = (Bitmap)Image.FromFile(@"Lenna.bmp", true);//load character set (digits 1->9..)
-            const int WIDTH = 1;
-            const int HEIGHT = 1;
-            bitmapColorsCached = new byte[image1.Width, image1.Height];
-
-            char[,] shaded = new char[40, 50];
 
             for (int i = 0; i < image1.Width; i += WIDTH)
             {
