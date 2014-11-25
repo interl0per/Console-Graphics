e5cdf5452d3b5fe690ea80d1d97b7f53514aee53
interl0per <cobatltking6@gmail.com>
2014-11-25 16:43:53 -0500

    Now with solid/textured rendering!
    (and a broken z buffer)

--- Rasterize.cs
+++ Rasterize.cs
@@ -1,9 +1,10 @@
-/* Rasterization - all the grunt work!
- * Determine pixle 'colors'..
+/* Determine pixle 'colors'..
  * 
  */
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 
@@ -27,15 +28,15 @@ namespace ConsoleGraphics
                 for (int i = 0; i < 3; i++)//each vertex
                 {
                     double depth = someShape.verts[f.vertIDs[i] - 1].z;
-                    int P_x1 = (int)((RENDER_WIDTH / 2) + someShape.verts[f.vertIDs[i] - 1].x / -depth);
-                    int P_y1 = (int)(((RENDER_HEIGHT - 15) / 2) + someShape.verts[f.vertIDs[i] - 1].y / -depth);
+                    int P_x1 = (int)((RENDER_WIDTH / 2) + someShape.verts[f.vertIDs[i] - 1].x / depth);
+                    int P_y1 = (int)(((RENDER_HEIGHT - 15) / 2) + someShape.verts[f.vertIDs[i] - 1].y / depth);
 
                     if (P_y1 < RENDER_HEIGHT - 1 && P_y1 >= 0 && P_x1 < RENDER_WIDTH - 1 && P_x1 >= 0)//the vert is in bounds with the screen
                         for (int j = 0; j < 2; j++)//each of the 2 neighboring verts
                         {
                             double depth2 = someShape.verts[f.vertIDs[j] - 1].z;
-                            int P_x = (int)((RENDER_WIDTH / 2) + someShape.verts[f.vertIDs[j] - 1].x / -depth2);
-                            int P_y = (int)(((RENDER_HEIGHT - 15) / 2) + someShape.verts[f.vertIDs[j] - 1].y / -depth2);
+                            int P_x = (int)((RENDER_WIDTH / 2) + someShape.verts[f.vertIDs[j] - 1].x / depth2);
+                            int P_y = (int)(((RENDER_HEIGHT - 15) / 2) + someShape.verts[f.vertIDs[j] - 1].y / depth2);
 
                             if (P_y < RENDER_HEIGHT - 1 && P_y >= 0 && P_x < RENDER_WIDTH - 1 && P_x >= 0)
                                 image = drawLine(image, P_x, P_y, P_x1, P_y1, true);
@@ -61,33 +62,45 @@ namespace ConsoleGraphics
 
             foreach (Mesh.triangle f in someShape.faces)
             {
-                Mesh.point3 surfaceNorm = Matrix.cross(someShape.verts[f.vertIDs[0] - 1], someShape.verts[f.vertIDs[1] - 1]);
-                Mesh.point3 lightNorm = Matrix.add(new Mesh.point3(0, 10, 0), someShape.verts[f.vertIDs[0] - 1]);//arbitrary thing
-                float lighting = Matrix.dot(surfaceNorm, lightNorm);
+                Mesh.point3 surfaceNorm = Matrix.cross(someShape.verts[f.vertIDs[0] - 1], someShape.verts[f.vertIDs[2] - 1]);
+                surfaceNorm = Matrix.normalize(surfaceNorm);
 
+                Mesh.point3 lightNorm = Matrix.add(new Mesh.point3(0, 0, 10), someShape.verts[f.vertIDs[2] - 1]);//arbitrary vert
+                lightNorm = Matrix.normalize(lightNorm);
+
+                float lighting = 0;// 100 * Math.Max(0, Matrix.dot(surfaceNorm, lightNorm));
+
+                //perspective projection
                 double depth1 = someShape.verts[f.vertIDs[0] - 1].z;
-                int x0 = (int)((RENDER_WIDTH / 2) + someShape.verts[f.vertIDs[0] - 1].x / -depth1);
-                int y0 = (int)(((RENDER_HEIGHT - 15) / 2) + someShape.verts[f.vertIDs[0] - 1].y / -depth1);
-
                 double depth2 = someShape.verts[f.vertIDs[1] - 1].z;
-                int x1 = (int)((RENDER_WIDTH / 2) + someShape.verts[f.vertIDs[1] - 1].x / -depth2);
-                int y1 = (int)(((RENDER_HEIGHT - 15) / 2) + someShape.verts[f.vertIDs[1] - 1].y / -depth2);
-
                 double depth3 = someShape.verts[f.vertIDs[2] - 1].z;
-                int x2 = (int)((RENDER_WIDTH / 2) + someShape.verts[f.vertIDs[2] - 1].x / -depth3);
-                int y2 = (int)(((RENDER_HEIGHT - 15) / 2) + someShape.verts[f.vertIDs[2] - 1].y / -depth3);
 
-                if (true)//((y0 < RENDER_HEIGHT && y0 >= 0 && x0 < RENDER_WIDTH && x0 > 0) || (y1 < RENDER_HEIGHT && y1 >= 0 && x1 < RENDER_WIDTH && x1 >= 0) || (y2 < RENDER_HEIGHT && y2 >= 0 && x2 < RENDER_WIDTH && x2 >= 0))
-                {   //>= one vertex is in screen
-                    //byte col = (byte)(Matrix.dot(Matrix.cross(someShape.verts[f.vertIDs[0] - 1], someShape.verts[f.vertIDs[1] - 1]), new Mesh.point3(-200, 10, 3)));
-                    image = drawTriangle(image, new Mesh.point3((float)x0, (float)y0, (float)depth1), new Mesh.point3((float)x1, (float)y1, (float)depth2), new Mesh.point3((float)x2, (float)y2, (float)depth3));
+                if (depth1 < 0 && depth2 < 0 && depth3 < 0)//((y0 < RENDER_HEIGHT && y0 >= 0 && x0 < RENDER_WIDTH && x0 > 0) || (y1 < RENDER_HEIGHT && y1 >= 0 && x1 < RENDER_WIDTH && x1 >= 0) || (y2 < RENDER_HEIGHT && y2 >= 0 && x2 < RENDER_WIDTH && x2 >= 0))
+                {
+                    int x0 = (int)((RENDER_WIDTH / 2) + someShape.verts[f.vertIDs[0] - 1].x / depth1);
+                    int y0 = (int)(((RENDER_HEIGHT - 15) / 2) + someShape.verts[f.vertIDs[0] - 1].y / depth1);
+
+                    int x1 = (int)((RENDER_WIDTH / 2) + someShape.verts[f.vertIDs[1] - 1].x / depth2);
+                    int y1 = (int)(((RENDER_HEIGHT - 15) / 2) + someShape.verts[f.vertIDs[1] - 1].y / depth2);
+
+                    int x2 = (int)((RENDER_WIDTH / 2) + someShape.verts[f.vertIDs[2] - 1].x / depth3);
+                    int y2 = (int)(((RENDER_HEIGHT - 15) / 2) + someShape.verts[f.vertIDs[2] - 1].y / depth3);
+
+                    //>= one vertex is in screen
+                    byte col = (byte)lighting;
+                    Mesh.point2 uv0 = someShape.uvVerts[f.vertIDs[0] - 1];
+                    Mesh.point2 uv1 = someShape.uvVerts[f.vertIDs[1] - 1];
+                    Mesh.point2 uv2 = someShape.uvVerts[f.vertIDs[2] - 1];
+                    Mesh.point2[] uvs = new Mesh.point2[] { uv0, uv1, uv2 };
+                    image = drawTriangle(image, new Mesh.point3((float)x0, (float)y0, (float)depth1), new Mesh.point3((float)x1, (float)y1, (float)depth2), new Mesh.point3((float)x2, (float)y2, (float)depth3), uvs,col);
+
                 }
             }
             z = Environment.TickCount - z;
             return (image);
         }
 
-        private byte[,] drawTriangle(byte[,] image, Mesh.point3 p1, Mesh.point3 p2, Mesh.point3 p3, byte color = 0)
+        private byte[,] drawTriangle(byte[,] image, Mesh.point3 p1, Mesh.point3 p2, Mesh.point3 p3, Mesh.point2[] uvs, byte color = 0)
         {
             /*
              *      A
@@ -150,7 +163,6 @@ namespace ConsoleGraphics
         {
             float invslope1 = (float)(p3.x - p1.x) / (float)(p3.y - p1.y);
             float invslope2 = (float)(p3.x - p2.x) / (float)(p3.y - p2.y);
-
             float curx1 = p3.x;
             float curx2 = p3.x;
 
@@ -180,7 +192,7 @@ namespace ConsoleGraphics
             return (image);
         }
 
-        public byte[,] drawLine(byte[,] image, int x0, int y0, int x1, int y1, bool drawWireFrame, Mesh.point3 a = new Mesh.point3(), Mesh.point3 b = new Mesh.point3(), Mesh.point3 c = new Mesh.point3(), byte color = 0)
+        public byte[,] drawLine(byte[,] image, int x0, int y0, int x1, int y1, bool drawWireFrame, Mesh.point3 a = new Mesh.point3(), Mesh.point3 c = new Mesh.point3(), Mesh.point3 b = new Mesh.point3(), byte color = 0)
         {
             float rise = y1 - y0;
             float run = x1 - x0;
@@ -209,18 +221,24 @@ namespace ConsoleGraphics
                         float b2 = (((c.y - a.y) * (x0 - c.x)) + ((a.x - c.x) * (y0 - c.y))) / denom;
                         float b3 = 1 - b1 - b2;
 
-                        float u = b1;
-                        float v = b2;
-                        int xpos = Math.Min(Math.Max((int)(u * 128), 0), 127);
-                        int ypos = Math.Min(Math.Max((int)(v * 128), 0), 127);
-
-                        byte fa = ASCIIRender.bitmapColorsCached[xpos, ypos];
-                        image[x0, y0] = (byte)Convert.ToInt32(ASCIIRender.levels[fa]);
+                        int interpolatedZ = (int)(a.z * b1 + b.z * b2 + c.z * b3);
+                        if (interpolatedZ > zBuffer[x0, y0])
+                        {
+                            float u = b1;
+                            float v = b2;
+                            int xpos = Math.Min(Math.Max((int)(u * 128), 0), 127);
+                            int ypos = Math.Min(Math.Max((int)(v * 128), 0), 127);
+                            byte fa = ASCIIRender.bitmapColorsCached[xpos, ypos];
+                            image[x0, y0] = (byte)(Convert.ToInt32(ASCIIRender.levels[fa])-color);
+                            zBuffer[x0, y0] = interpolatedZ;
+                        }
                     }
                 }
                 return (image);
             }
+
             float m = rise / run;
+
             if (-1 <= rise / run && rise / run <= 1)
             {
                 if (drawWireFrame)
@@ -261,17 +279,17 @@ namespace ConsoleGraphics
                             float b1 = (((b.y - c.y) * (x0 - c.x)) + ((c.x - b.x) * (y - c.y))) / denom;
                             float b2 = (((c.y - a.y) * (x0 - c.x)) + ((a.x - c.x) * (y - c.y))) / denom;
                             float b3 = 1 - b1 - b2;
-
-                            float u = b1;
-                            float v = b2;
-                            int xpos = Math.Min(Math.Max((int)(u * 128), 0), 127);
-                            int ypos = Math.Min(Math.Max((int)(v * 128), 0), 127);
-
-                            byte fa = ASCIIRender.bitmapColorsCached[xpos, ypos];
                             int interpolatedZ = (int)(a.z * b1 + b.z * b2 + c.z * b3);
                             if (interpolatedZ > zBuffer[x0, (int)y])
                             {
-                                image[x0, (int)y] = (byte)Convert.ToInt32(ASCIIRender.levels[fa]);
+                                float u = b1;
+                                float v = b2;
+                                int xpos = Math.Min(Math.Max((int)(u * 128), 0), 127);
+                                int ypos = Math.Min(Math.Max((int)(v * 128), 0), 127);
+
+                                byte fa = ASCIIRender.bitmapColorsCached[xpos, ypos];
+
+                                image[x0, (int)y] = (byte)(Convert.ToInt32(ASCIIRender.levels[fa])-color);
                                 zBuffer[x0, (int)y] = interpolatedZ;
                             }
                         }
