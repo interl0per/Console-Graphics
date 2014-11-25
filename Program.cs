5c74c2d2fdbfe73e1eb941b33647101e74914d73
interl0per <cobatltking6@gmail.com>
2014-11-25 16:37:40 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using System.IO;
-using ConsoleExtender;
 using System.Drawing;
 
 namespace ConsoleGraphics
@@ -10,7 +12,8 @@ namespace ConsoleGraphics
     class Program
     {
         //special characters:█ ▄ ▀ ■
-        public bool VSYNC_ON = true;
+
+        public bool VSYNC_ON = false;
         public static bool renderWireFr = true;
         //    public bool FRAMERATE_ON = true;
         public const short RENDER_WIDTH = 300;
@@ -24,13 +27,12 @@ namespace ConsoleGraphics
             Console.Title = title;
             Console.CursorVisible = false;
             Console.BackgroundColor = ConsoleColor.Black;
-            ConsoleHelper.SetConsoleFont(0);
-            Console.SetBufferSize(width, height + 15);
             Console.SetWindowSize(width, height + 15);
+            Console.SetBufferSize(width, height + 15);
             Console.Clear();//clear colors from user preset.
             Console.SetCursorPosition(0, height);
             Console.Write(new String('▄', RENDER_WIDTH));
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Cyan;
 
             Bitmap image1 = (Bitmap)Image.FromFile(@"charset.bmp", true);//load character set (digits 1->9..)
             for (int i = 0; i < 9; i++)
@@ -48,15 +50,23 @@ namespace ConsoleGraphics
             }
             Thread cin = new Thread(getInput);
             cin.Start();
-            Thread soundPlayer = new Thread(playSound);
-            soundPlayer.Start();
+            //Thread soundPlayer = new Thread(playSound);
+            //soundPlayer.Start();
+        }
+
+        static byte[,] loadTex(string loc)
+        {
+            byte[,] loadedTexture = new byte[2, 2];
+            return (loadedTexture);
         }
 
         static Mesh loadObj(string loc)
         {
             StreamReader r = new StreamReader(loc + ".obj");
             List<Mesh.point3> loadedVerts = new List<Mesh.point3>();
+            List<Mesh.point2> loadedUvs = new List<Mesh.point2>();
             List<Mesh.triangle> loadedFaces = new List<Mesh.triangle>();
+            List<Mesh.triangle> loadedUvFaces = new List<Mesh.triangle>();
 
             int vertCount = 0;
             int faceCount = 0;
@@ -66,12 +76,19 @@ namespace ConsoleGraphics
                 string e1 = r.ReadLine();
                 if (e1 != "")
                 {
-                    if (e1[0] == 'v')//vertex
+                    if (e1[0] == 'v' && e1[1] == 't')//texture verticies
+                    {
+                        string temp = e1.Substring(3, e1.Length - 3);
+                        string[] coords = temp.Split(' ');
+                        Mesh.point2 p = new Mesh.point2(float.Parse(coords[0]), float.Parse(coords[1]));
+                        loadedUvs.Add(p);
+                    }
+                    else if (e1[0] == 'v')//vertex
                     {
                         vertCount++;
                         string temp = e1.Substring(3, e1.Length - 3);
                         string[] coords = temp.Split(' ');
-                        Mesh.point3 p = new Mesh.point3(float.Parse(coords[0]) * 100, float.Parse(coords[1]) * 100, -float.Parse(coords[2]));
+                        Mesh.point3 p = new Mesh.point3(float.Parse(coords[0]) * 100, float.Parse(coords[1]) * 100, float.Parse(coords[2]) / 2);
                         //if z is positive it's off screen, z=0 we clip
                         loadedVerts.Add(p);
                     }
@@ -79,13 +96,15 @@ namespace ConsoleGraphics
                     {
                         faceCount++;
                         string temp = e1.Substring(2, e1.Length - 2);
-                        string[] coords = temp.Split(' ');
-                        Mesh.triangle f = new Mesh.triangle(int.Parse(coords[0]), int.Parse(coords[1]), int.Parse(coords[2]));
+                        string[] coords = temp.Split(' ', '/');
+                        Mesh.triangle f = new Mesh.triangle(int.Parse(coords[0]), int.Parse(coords[2]), int.Parse(coords[4]));
+                        Mesh.triangle uvf = new Mesh.triangle(int.Parse(coords[1]), int.Parse(coords[3]), int.Parse(coords[5]));
                         loadedFaces.Add(f);
+                        loadedUvFaces.Add(uvf);
                     }
                 }
             }
-            return (new Mesh(loadedVerts.ToArray(), loadedFaces.ToArray()));
+            return (new Mesh(loadedVerts.ToArray(), loadedFaces.ToArray(), loadedUvs.ToArray()));
         }
 
         static Mesh someShape = loadObj("box");
@@ -96,12 +115,12 @@ namespace ConsoleGraphics
             init(RENDER_WIDTH, RENDER_HEIGHT, "render");
             FrameBuffer buffer = new FrameBuffer();
             Rasterizer rast = new Rasterizer();
-            someShape.translate(new Mesh.point3(0, 10, 0));
+            someShape.translate(new Mesh.point3(0, -300, -8));
             //someShape.rotate((float)0.25);
 
             buffer.drawFrame(rast.renderSolid(someShape));
             Console.ReadLine();
-
+            //someShape.rotate((float)(3.1415));
             for (double i = 0; i < 1000; i += 0.01)
             {
                 //  buffer.drawFrame(drawLine(new byte[RENDER_HEIGHT * RENDER_WIDTH], a1, a2));
@@ -110,8 +129,7 @@ namespace ConsoleGraphics
                 else
                     buffer.drawFrame(rast.renderWire(someShape));
 
-                someShape.translate(new Mesh.point3(0, -(float)Math.Sin(i)/50, 0));
-                someShape.rotate((float)0.01);
+                // someShape.rotate((float)0.001);
             }
             Console.ReadLine();
         }
@@ -119,36 +137,27 @@ namespace ConsoleGraphics
         static void getInput()
         {
             ConsoleKeyInfo cki;
+            // Console.TreatControlCAsInput = true;
             do
             {
                 cki = Console.ReadKey();
-                renderWireFr = !renderWireFr;
+                if (cki.Key == ConsoleKey.Spacebar)
+                    renderWireFr = !renderWireFr;
+                else if (cki.Key == ConsoleKey.W)
+                    someShape.translate(new Mesh.point3(0, 0, 1));
+                else if (cki.Key == ConsoleKey.A)
+                    someShape.translate(new Mesh.point3(-10, 0, 0));
+                else if (cki.Key == ConsoleKey.S)
+                    someShape.translate(new Mesh.point3(0, 0, -1));
+                else if (cki.Key == ConsoleKey.D)
+                    someShape.translate(new Mesh.point3(10, 0, 0));
+                else if (cki.Key == ConsoleKey.R)
+                    someShape.rotate(0.06);
+                else if (cki.Key == ConsoleKey.Q)
+                    someShape.translate(new Mesh.point3(0, -10, 0));
+                else if (cki.Key == ConsoleKey.E)
+                    someShape.translate(new Mesh.point3(0, 10, 0));
             } while (cki.Key != ConsoleKey.Escape);
-        }
-
-        static void playSound()
-        {
-            while (true)
-            {
-                Console.Beep(446, 500);
-                Console.Beep(440, 500);
-                Console.Beep(440, 500);
-                Console.Beep(349, 350);
-                Console.Beep(523, 150);
-                Console.Beep(440, 500);
-                Console.Beep(349, 350);
-                Console.Beep(523, 150);
-                Console.Beep(440, 1000);
-                Console.Beep(659, 500);
-                Console.Beep(659, 500);
-                Console.Beep(659, 500);
-                Console.Beep(698, 350);
-                Console.Beep(523, 150);
-                Console.Beep(415, 500);
-                Console.Beep(349, 350);
-                Console.Beep(523, 150);
-                Console.Beep(440, 1000);
-            }
         }
 
         public static void printch(char[] charId, int x, int y)
