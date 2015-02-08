79b15d9859617193f99df8bf996ed555967be567
interl0per <cobaltking6@gmail.com>
2015-02-08 18:40:18 -0500

    Update Mesh.cs

--- Mesh.cs
+++ Mesh.cs
@@ -12,20 +12,22 @@ namespace ConsoleGraphics
 {
     class Mesh : Program
     {
-        public Mesh(point3[] meshVerts, triangle[] meshFaces, point2[] meshUvs = null)
+        public Mesh(point3[] meshVerts, triangle[] meshFaces, point2[] meshUvs, Material[] mtls = null)
         {
             verts = meshVerts;
             vertCount = verts.Length;
             faces = meshFaces;
             faceCount = faces.Length;
             uvVerts = meshUvs;
+            matIDs = mtls;
         }
 
         public struct triangle
         {
             public int[] vertIDs;
             public int[] uvIds;
-            public triangle(int v1, int v2, int v3, int vt1, int vt2, int vt3)
+            public int matID;
+            public triangle(int v1, int v2, int v3, int vt1, int vt2, int vt3, int mid)
             {
                 vertIDs = new int[3];
                 vertIDs[0] = v1;
@@ -35,6 +37,7 @@ namespace ConsoleGraphics
                 uvIds[0] = vt1;
                 uvIds[1] = vt2;
                 uvIds[2] = vt3;
+                matID = mid;
             }
         };
 
@@ -64,7 +67,7 @@ namespace ConsoleGraphics
         public point2[] uvVerts;
         public int vertCount;
         public int faceCount;
-
+        public Material[] matIDs;
         public void translate(point3 translation)
         {
             for (int i = 0; i < verts.Length; i++)
@@ -79,7 +82,8 @@ namespace ConsoleGraphics
             float cosAngle = (float)Math.Cos(angle);
             float sinAngle = (float)Math.Sin(angle);
             Matrix rotMtrx = null;
-
+            point3 startCoords = verts[0];
+            translate(new Mesh.point3(-startCoords.x, -startCoords.y, -startCoords.z));
             #region rotations
             /*        Rotate about X Axis            {{1,0,0},
                                                      {0,  cosAngle,sinAngle*100}, 
@@ -95,6 +99,7 @@ namespace ConsoleGraphics
                                                      {0,         0,       1}},3,3);
              */
             #endregion
+            int x = Environment.TickCount;
 
             if (dir == 0)
                 rotMtrx = new Matrix(new float[,] {{1,0,0},
@@ -108,11 +113,13 @@ namespace ConsoleGraphics
                 rotMtrx = new Matrix(new float[,] {{cosAngle,-sinAngle,0},
                                                      {sinAngle,  cosAngle,0}, 
                                                      {0,         0,       1}}, 3, 3);
+            x = Environment.TickCount - x;
 
             for (int i = 0; i < vertCount; i++)
             {
                 verts[i] = rotMtrx.multiply(verts[i]);
             }
+            translate(new Mesh.point3(startCoords.x, startCoords.y, startCoords.z));
         }
     }
 }
